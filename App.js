14950072503c403adbc73dc08d4fe9857464b373
Yash Rajput <rajputyash8297@gmail.com>
2026-05-07 08:23:52 +0530

    Episode 2 changes

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React",
-);
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading2" },
-  "Hello World from React",
-);
-const child1 = React.createElement("div", { id: "child1" }, [
-  heading,
-  heading2,
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement(
+      "h1",
+      { key: "c1-h1", className: "header" },
+      "Hello World from React",
+    ),
+    React.createElement(
+      "h2",
+      { key: "c1-h2", className: "header" },
+      "Hello World from React",
+    ),
+  ]),
+
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement(
+      "h1",
+      { key: "c2-h1", className: "header" },
+      "Hello World from React 😎",
+    ),
+    React.createElement(
+      "h2",
+      { key: "c2-h2", className: "header" },
+      "Hello World from React",
+    ),
+  ]),
 ]);
-const child2 = React.createElement("div", { id: "child2" }, [
-  heading,
-  heading2,
-]);
-const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+
 root.render(parent);
